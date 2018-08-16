077cd4ab087030dd5faec1b9378bf5ee4b9edfc3
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-16 00:31:03 -0700

    Remove authentication

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ $router->get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 $router->get('/home', 'HomeController@index')->name('home');
 
 $router->get('home/{any}', 'HomeController@index')->where('any', '.*');
-
-$router->redirect('/register', '/login');
